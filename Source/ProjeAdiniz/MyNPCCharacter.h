836e6cde165052c983c8934ef69f44cee7a7d756
Elif Hakarar <hakararelf@gmail.com>
2026-09-15 20:19:01 +0300

    Add MyNPCCharacter class header file

---
+++ Source/ProjeAdiniz/MyNPCCharacter.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "MyNPCCharacter.generated.h"
+
+UCLASS()
+class MYGAME_API AMyNPCCharacter : public ACharacter
+{
+    GENERATED_BODY()
+
+public:
+    AMyNPCCharacter();
+};
